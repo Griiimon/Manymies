9bbeb695aa71a2696a1da2d0233fe2c984ef368e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-09-25 21:13:55 +0200

    option to pause when dropping below n FPS

--- main.gd
+++ main.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+
+@export var pause_below_n_fps: int= 40
 @onready var enemies: Node2D = $Enemies
 
 
 
 func _ready() -> void:
 	Global.enemies= enemies
+
+
+func _process(delta: float) -> void:
+	assert(Engine.get_process_frames() < 500 or Engine.get_frames_per_second() >= pause_below_n_fps, "Dropped to %d FPS with %d enemies" % [Engine.get_frames_per_second(), enemies.get_child_count()])
