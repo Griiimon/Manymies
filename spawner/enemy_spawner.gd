2b0e68aab02254b82aa7249f2bbb5d3880e4fe6a
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-10-05 04:15:24 +0200

    wait for empty space option

--- spawner/enemy_spawner.gd
+++ spawner/enemy_spawner.gd
@@ -2,16 +2,32 @@ extends Node2D
 
 @export var enemy_scene: PackedScene
 @export var spawn_interval: float= 0.1
+@export var wait_for_empty_space: bool= false
+@export var empty_radius: float= 20.0
 
 @onready var timer: Timer = $Timer
 
 var paused:= false
-
+var busy:= false
+var empty_space_query: PhysicsShapeQueryParameters2D
 
 
 func _ready() -> void:
+	if wait_for_empty_space:
+		var query:= PhysicsShapeQueryParameters2D.new()
+		query.collide_with_areas= true
+		query.collide_with_bodies= true
+		query.collision_mask= Global.ENEMY_COLLISION_LAYER + Global.RAYCAST_ENEMY_COLLISION_LAYER
+		query.transform= transform
+		var shape:= CircleShape2D.new()
+		shape.radius= empty_radius
+		query.shape= shape
+		empty_space_query= query
+
 	timer.wait_time= spawn_interval
 	timer.start()
+
+
 
 
 func _process(delta: float) -> void:
@@ -20,7 +36,17 @@ func _process(delta: float) -> void:
 
 
 func spawn_enemy():
+	if busy: return
 	if paused: return
+	if wait_for_empty_space:
+		busy= true
+		await get_tree().physics_frame
+		busy= false
+		var space_state: PhysicsDirectSpaceState2D= get_world_2d().direct_space_state
+		if not space_state: return
+		var result= space_state.intersect_shape(empty_space_query)
+		if result: return
+		
 	var obj: BaseEnemy= enemy_scene.instantiate()
 	obj.position= position
 	Global.enemies.add_child(obj)
