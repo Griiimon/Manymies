f2bea1baab45e11f7909e40e30a686d4ad6034a8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-10-05 05:09:13 +0200

    switch to RaycastHelper

--- solutions/raycast enemy/raycast_enemy.gd
+++ solutions/raycast enemy/raycast_enemy.gd
@@ -9,30 +9,46 @@ extends BaseEnemy
 # for smoother movement
 @export_range(0.0, 1.0) var jitter_fix= 0.5
 
+@export var ray_length: float= 25.0
 @export var num_side_raycasts: int= 1
-
 @export var raycast_angle: float= 45.0
 
 @onready var head: Node2D = $Head
-@onready var straight_raycast: RayCast2D = %"RayCast2D Straight"
+
+static var helper_initialized: bool= false
 
 var velocity: Vector2
 var tick_offset: int
+var center_marker: Marker2D
+
 
 
 func _ready():
 	tick_offset= randi() % 60
 
+	if not helper_initialized:
+		RaycastHelper.collide_with_areas= true
+		RaycastHelper.collide_with_bodies= true
+		RaycastHelper.collision_mask= Global.PLAYER_COLLISION_LAYER + Global.OBSTACLE_COLLISION_LAYER + Global.RAYCAST_ENEMY_COLLISION_LAYER
+		helper_initialized= true
+
+
+	var marker:= Marker2D.new()
+	marker.position= head.transform.x * ray_length
+	head.add_child(marker)
+	center_marker= marker
+
 	var angle:= 0.0
+
 	for i in num_side_raycasts:
 		angle+= raycast_angle
-		var new_raycast: RayCast2D= straight_raycast.duplicate()
-		new_raycast.target_position= new_raycast.target_position.rotated(deg_to_rad(angle))
-		head.add_child(new_raycast)
+		marker= Marker2D.new()
+		marker.position= head.transform.x.rotated(deg_to_rad(angle)) * ray_length
+		head.add_child(marker)
 
-		new_raycast= straight_raycast.duplicate()
-		new_raycast.target_position= new_raycast.target_position.rotated(deg_to_rad(-angle))
-		head.add_child(new_raycast)
+		marker= Marker2D.new()
+		marker.position= head.transform.x.rotated(deg_to_rad(-angle)) * ray_length
+		head.add_child(marker)
 
 
 
@@ -50,18 +66,19 @@ func _physics_process(delta: float) -> void:
 		var steer_vec: Vector2= Vector2.ZERO
 		var center_blocked:= false
 		
-		for raycast: RayCast2D in head.get_children():
-			if not raycast.is_colliding():
-				steer_vec+= raycast.target_position.rotated(raycast.global_rotation)
-			elif raycast == straight_raycast:
+		for marker: Marker2D in head.get_children():
+			RaycastHelper.update(head.global_position, marker.global_position)
+			if not RaycastHelper.is_colliding():
+				steer_vec+= (marker.global_position - head.global_position).normalized()#raycast.target_position.rotated(raycast.global_rotation)
+			elif marker == center_marker:
 				center_blocked= true
 
 		steer_vec= steer_vec.normalized()
 		
-		if center_blocked: 
-			if steer_vec.is_equal_approx(straight_raycast.target_position.rotated(straight_raycast.global_rotation).normalized()):
-				steer_vec= Vector2.ZERO
-				rotate(randf_range(-0.1, 0.1))
+		#if center_blocked: 
+			#if steer_vec.is_equal_approx(straight_raycast.target_position.rotated(straight_raycast.global_rotation).normalized()):
+				#steer_vec= Vector2.ZERO
+				#rotate(randf_range(-0.1, 0.1))
 					
 		velocity= maximum_speed * steer_vec
 	
